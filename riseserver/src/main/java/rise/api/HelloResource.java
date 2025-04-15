51c4caa4bfa66359630ad85d861d95f33098b447
leonevalentina <valentina.leone@wasdi.cloud>
2025-04-15 15:10:42 +0200

    add endpoint to try shellexec

--- riseserver/src/main/java/rise/api/HelloResource.java
+++ riseserver/src/main/java/rise/api/HelloResource.java
@@ -1,13 +1,22 @@
 package rise.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import rise.lib.business.Organization;
+import rise.lib.config.RiseConfig;
+import rise.lib.utils.RunTimeUtils;
+import rise.lib.utils.ShellExecReturn;
 import rise.lib.utils.Utils;
 import rise.lib.utils.log.RiseLog;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
 
 /**
  * Root resource (exposed at "hello" path)
@@ -40,5 +49,20 @@ public class HelloResource {
     	oOrganization.setCity("Dudelange");
     	
     	return oOrganization;
+    }
+    
+    @GET
+    @Path("pythonScript")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response launchPythonScript(@QueryParam("pythonScriptPath") String sPythonScript) {
+    	List<String> asArgs = new ArrayList<String>();
+    	asArgs.add(sPythonScript);
+    	asArgs.add("me");
+    	ShellExecReturn oReturn = RunTimeUtils.shellExec(asArgs, true, true);
+    	System.out.println(oReturn.getOperationLogs());
+    	System.out.println(oReturn.getOperationReturn());
+    	return Response.ok().build();
+    	
+    	// return Response.status(Status.INTERNAL_SERVER_ERROR).build();
     }
 }
